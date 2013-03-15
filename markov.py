7adafbbf068fce1f1cae02f6b3f07945e9006e3c
Hackbright Student <user@mini-001.(none)>
2013-03-14 17:22:24 -0700

    working with single words

--- markov.py
+++ markov.py
@@ -1,40 +1,56 @@
 #!/usr/bin/env python
 
 import sys
+import random
+from random import randint
 
 def make_chains(corpus):
-    """Takes an input text as a string and returns a dictionary of
-    markov chains."""
-    dictionary = {}
-    corpus = corpus.split()
-    corpus.append(" ")
-    i = 1
-    for words in corpus:
-        dictionary[words] = corpus[i]
-        i += 1
-        if i == (len(corpus) - 1):
-            break
-    print dictionary
+	"""Takes an input text as a string and returns a dictionary of
+	markov chains."""
+	dictionary = {}
+	corpus = corpus.lower().split()
+	i = 1
+	for words in corpus:
+		if words not in dictionary:
+			#dictionary[words] is the value
+			dictionary[words] = [corpus[i]]
+		else:
+			#appends changes the list
+			dictionary[words].append(corpus[i])
+		i += 1
+		if i == (len(corpus) - 1):
+			break
 
-    return dictionary
+	print "Dictionary", dictionary
+	return dictionary
 
 def make_text(chains):
-    """Takes a dictionary of markov chains and returns random text
-    based off an original text."""
-    return "Here's some random text."
+
+	# def next_word(dictionary, current_word):
+	# 	next_word = dictionary[first_word][randint(0, len(dictionary[first_word]) - 1)]
+	# 	return next_word
+
+	"""Takes a dictionary of markov chains and returns random text
+	based off an original text."""
+	for pair in range(0, 4):
+		first_word = random.choice(chains.keys())
+		#random_text = chains[first_word] #this value is an array
+		random_int = chains[first_word][randint(0, len(chains[first_word]) - 1)]
+		print first_word, random_int,
+	# random_text = chains[first_word[randint(0,len(chains[first_word])]
+	# first_word = first_word + next_word(chains, first_word)
+
 
 def main(argv):
-    args = sys.argv
-    # argv = script, filename
-    script, filename = argv
+	args = sys.argv
+	script, filename = argv
 
-    #opens txt file form command line
-    input_text = open(filename).read()
-    make_chains(input_text)
+	#opens txt file form command line
+	input_text = open(filename).read()
+	dictionary = make_chains(input_text)
 
-    chain_dict = make_chains(input_text)
-    #random_text = make_text(chain_dict)
-    #print random_text
+	random_text = make_text(dictionary)
+	#print random_text
 
 if __name__ == "__main__":
-    main(sys.argv)
+	main(sys.argv)
