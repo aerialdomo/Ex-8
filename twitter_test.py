f3af86d604f46c507c18f7cea39928ddcde77dcc
Christina Liu <christinaliu26@gmail.com>
2013-04-24 14:19:11 -0700

    Update twitter_test.py

--- twitter_test.py
+++ twitter_test.py
@@ -1,10 +1,10 @@
 import twitter
 import markov
 
-api = twitter.Api(consumer_key = 'Mh9QZzIWqBcnSeQAifWGQ',
-                  consumer_secret ='2Isj9UvjaUysZlHBVQlZmf14iWloDq6GCqQSNIO4',
-                  access_token_key='1279013186-ohmMnlHVbWz96eISVytcgjauvFSzKAfdc8oLjrV',
-                  access_token_secret='jlAhKY3qaWvc1O2qyCy1LWfrfPMLcOT16D2ZwLDVLuQ')
+api = twitter.Api(consumer_key = xxx,
+                  consumer_secret =xxx,
+                  access_token_key=xxx,
+                  access_token_secret=xxx
 
 status = api.PostUpdate(tweet)
 print status.text
